1f7e0d41465f9a457313ca18e8f6f4f1ff36632e
Sven Scharmentke <sven.scharmentke@liasoft.de>
2024-03-27 08:48:30 +0100

    Update CMakeLists and introduce conanfile.

---
+++ conanfile.py
@@ -0,0 +1,47 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
+
+
+class sr25519Recipe(ConanFile):
+    name = "sr25519"
+    version = "1.0.0"
+
+    # Optional metadata
+    license = "Apache-2.0 license"
+    author = "sr25519"
+    url = "https://github.com/svnscha/sr25519"
+    description = "Pure C implementation of schnorrkel algorithm (SR25519), forked from TerenceGe/sr25519-donna."
+    topics = ("crypto", "sr25519", "snorks")
+
+    # Binary configuration
+    settings = "os", "compiler", "build_type", "arch"
+
+    options = {"shared": [True, False]}
+    default_options = {"shared": False }
+
+    # Sources are located in the same place as this recipe, copy them to the recipe
+    exports_sources = "CMakeLists.txt", "src/*"
+
+    def config_options(self):
+        pass
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["sr25519"]
